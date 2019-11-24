b7f75aae54fdc5640852f9ab4c9708e8ec1c8a56
shubham860 <shubham.chauhan860@gmail.com>
2019-11-25 02:57:01 +0530

    commit-8

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter'
-import PrevCounter from './components/PrevCounter'
-import UsestateObject from './components/UsestateObject'
-import UsestateArray from './components/UsestateArray'
-import UseeffectRender from './components/UseeffectRender';
-import Useeffect_once from './components/Useeffect_once';
-import UseeffectCleanup from './components/UseeffectCleanup';
-import FetchingData from './components/FetchingData'
+// import Counter from './components/useState/Counter'
+import PrevCounter from './components/useState/PrevCounter'
+import UsestateObject from './components/useState/UsestateObject'
+import UsestateArray from './components/useState/UsestateArray'
+import UseeffectRender from './components/useEffect/UseeffectRender';
+import Useeffect_once from './components/useEffect/Useeffect_once';
+import UseeffectCleanup from './components/useEffect/UseeffectCleanup';
+import FetchingData2 from './components/useEffect/FetchingData2'
+import ComponentC from "./components/Context_api/ComponentC";
+import {UserProvider,ChannelProvider} from "./components/Context_api/ContextApi";
+import Counter from './components/useReducer/Counter'
 
 function App() {
   return (
@@ -19,7 +22,13 @@ function App() {
       {/*{<UseeffectRender/>}*/}
       {/*{<Useeffect_once/>}*/}
       {/*<UseeffectCleanup/>*/}
-      <FetchingData/>
+      {/*<FetchingData2/>*/}
+      {/*<UserProvider value={'shubham'}>*/}
+      {/*    <ChannelProvider value={"code"}>*/}
+      {/*        <ComponentC/>*/}
+      {/*    </ChannelProvider>*/}
+      {/*</UserProvider>*/}
+      <Counter/>
     </div>
   );
 }
